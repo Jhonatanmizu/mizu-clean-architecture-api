69de2276a14b0150987f77a2e91bdec244728a4b
jhonatanmizu <natanjesuss20@gmail.com>
2024-06-07 20:13:37 -0300

    chore: setup husky

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from "vitest";
+import { UserData } from "./user-data";
+
+describe("Register user on mailing list use case", () => {
+  it("should add user with complete data to mailing list", async () => {
+    const users: UserData[] = [];
+    expect(users).toBeInstanceOf(Array);
+    // const repo: UserRepository = new InMemoryUserRepository(users);
+    // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(
+    //   repo
+    // );
+    // const name = "John doe";
+    // const email = "any@email.com";
+    // const response = await usecase.registerUserOnMailingList({ name, email });
+    // const user = await repo.findUserByEmail("any@email.com");
+    // expect(user).toBeDefined();
+    // expect(user.name).toEqual(name);
+    // expect(user.email).toEqual(email);
+  });
+});
