8c94b3a714a46b31d6b088b184a02342e311e568
jhonatanmizu <natanjesuss20@gmail.com>
2024-06-08 17:40:27 -0300

    test: finding, existence, addition

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -9,4 +9,33 @@ describe("In Memory User Repository", () => {
     const user = await userRepo.findUserByEmail("example@mail.com");
     expect(user).toBe(null);
   });
+
+  it("should return user if it is found", async () => {
+    const users: UserData[] = [];
+    const userRepo = new InMemoryUserRepository(users);
+    const userData: UserData = { name: "john doe", email: "johndoe@gmail" };
+    await userRepo.add(userData);
+    const user = await userRepo.findUserByEmail(userData.email);
+    expect(user).toBeDefined();
+    expect(userData.name).toEqual(user?.name);
+    expect(userData.email).toEqual(user?.email);
+  });
+
+  it("should return a empty array when does not have users", async () => {
+    const users: UserData[] = [];
+    const userRepo = new InMemoryUserRepository(users);
+    const result = await userRepo.findAllUsers();
+    expect(result).toBeInstanceOf(Array);
+    expect(result.length).toBe(0);
+  });
+
+  it("should return a users array", async () => {
+    const users: UserData[] = [];
+    const userRepo = new InMemoryUserRepository(users);
+    const result = await userRepo.findAllUsers();
+    const userData: UserData = { name: "john doe", email: "johndoe@gmail" };
+    await userRepo.add(userData);
+    expect(result).toBeInstanceOf(Array);
+    expect(result.length).toBe(1);
+  });
 });
