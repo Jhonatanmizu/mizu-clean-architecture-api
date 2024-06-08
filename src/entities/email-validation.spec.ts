a494d9ef48f481c9ae014dea24214ca31711f28d
jhonatanmizu <natanjesuss20@gmail.com>
2024-06-08 19:45:43 -0300

    test: email-validation cases

---
+++ src/entities/email-validation.spec.ts
@@ -0,0 +1,58 @@
+import { describe, expect, it } from "vitest";
+import { Email } from "./email";
+
+describe("Email Validation", () => {
+  it("should not accept empty strings", () => {
+    const email = "";
+    const result = Email.validate(email);
+    expect(result).toBeFalsy();
+  });
+
+  it("should not accept local part with invalid char", () => {
+    const email = "any @gmail.com";
+    const result = Email.validate(email);
+    expect(result).toBeFalsy();
+  });
+
+  it("should not accept domain part with invalid char", () => {
+    const email = "any@ gmail.com";
+    const result = Email.validate(email);
+    expect(result).toBeFalsy();
+  });
+
+  it("should not accept local part larger than 64 chars", () => {
+    const email = "l".repeat(65) + "gmail.com";
+    const result = Email.validate(email);
+    expect(result).toBeFalsy();
+  });
+
+  it("should not accept strings larger than 320 chars", () => {
+    const email =
+      "l".repeat(64) + "@" + "d".repeat(128) + "." + "d".repeat(127);
+    const result = Email.validate(email);
+    expect(result).toBeFalsy();
+  });
+
+  it("should not accept domain part larger than 255 chars", () => {
+    const email = "local@" + "d".repeat(128) + "." + "d".repeat(127);
+    const result = Email.validate(email);
+    expect(result).toBeFalsy();
+  });
+
+  it("should not accept empty local part", () => {
+    const email = "@gmail.com";
+    const result = Email.validate(email);
+    expect(result).toBeFalsy();
+  });
+  it("should not accept empty domain part", () => {
+    const email = "local@";
+    const result = Email.validate(email);
+    expect(result).toBeFalsy();
+  });
+
+  it("should not accept domain with a part larger than 63 chars", () => {
+    const email = "local@" + "d".repeat(64) + ".com";
+    const result = Email.validate(email);
+    expect(result).toBeFalsy();
+  });
+});
