f94c521059324c51abd66adc242c1263a9797b0a
jhonatanmizu <natanjesuss20@gmail.com>
2024-06-30 21:55:16 -0300

    fix: incorrect email regex

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -9,7 +9,8 @@ export class Email {
   }
   static validate(email: string): boolean {
     const emailRegex =
-      /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}]~(\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+      /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+
     if (!email) return false;
     const [local, domain] = email.split("@");
 
