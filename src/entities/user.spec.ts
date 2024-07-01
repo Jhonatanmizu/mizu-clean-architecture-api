4fc5041451156d68d000245f5cc8cb1df5bc474c
jhonatanmizu <natanjesuss20@gmail.com>
2024-06-30 21:29:34 -0300

    test: verify name while creating user

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -2,6 +2,7 @@ import { describe, expect, it } from "vitest";
 import { left } from "../shared/either";
 import { InvalidEmailError } from "./errors/invalid-email-error";
 import { User } from "./user";
+import { InvalidNameError } from "./errors/invalid-name-error";
 
 describe("User domain entity", () => {
   it("should not create user with invalid e-mail address", () => {
@@ -11,5 +12,27 @@ describe("User domain entity", () => {
       email: invalidEmail,
     });
     expect(error).toEqual(left(new InvalidEmailError()));
+  });
+
+  it("should not create user with invalid name (too few characters) ", () => {
+    const invalidName = "O";
+
+    const error = User.create({
+      name: invalidName,
+      email: "valid@gmail.com",
+    });
+
+    expect(error).toEqual(left(new InvalidNameError()));
+  });
+
+  it("should not create user with invalid name (too many characters", () => {
+    const invalidName = "O".repeat(257);
+
+    const error = User.create({
+      name: invalidName,
+      email: "valid@gmail.com",
+    });
+
+    expect(error).toEqual(left(new InvalidNameError()));
   });
 });
